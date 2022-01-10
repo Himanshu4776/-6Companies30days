d42012b420e6e18fdc12f64ceb4c9f1b8538bd8c
Himanshu4776 <hk91619@gmail.com>
2022-01-10 13:57:26 +0530

    microsoft commit 1

--- microsoft/microsoft-1.cpp
+++ microsoft/microsoft-1.cpp
@@ -1,3 +1,37 @@
 // Question -1 :
 // https://practice.geeksforgeeks.org/problems/minimum-sum-partition3317/1/
 
+#include<iostream>
+using namespace std;
+
+class solution
+{
+public:
+	int minDifference(int arr[], int n)
+	{
+		return 0;
+	}
+	
+};
+
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		int a[n];
+		for (int i = 0; i < n; ++i)
+			cin>>a[i];
+
+		solution ob;
+		cout<<ob.minDifference(a,n)<<endl;		
+	}
+	/* code */
+	return 0;
+}
